6f897aef25a82c628f80ad1019ea73a06517ac54
kotobukid <kotobuki@daqua.jp>
2021-05-16 18:05:34 +0900

    GithubGuardianがsecret文字列が公開されていると指摘してくれたので、問題ない旨を記述する

--- djadmin/djadmin/settings.py
+++ djadmin/djadmin/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$@g41a01le*-v!%&rpj4-f#ooysqw9@-s*$$(qs9tgrqlov)fq'
+SECRET_KEY = 'this-django-is-only-for-db-management(published for private).'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
